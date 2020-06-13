661164349be1f0fc4a7d1bc09821aedcd7580a4a
sergio <sbarajas510@gmail.com>
2020-06-13 13:25:27 -0700

    done

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const fav = require('./fav');
 const app = express();
 app.use(fav);
 
-
+app.use((req, res, next) => {
+    console.log('Hello');
+    const err = new Error('Holy Smokes!');
+    next(err);
+});
+app.use((req, res, next) => {
+    console.log('World');
+    next();
+    });
 
 app.use((req, res) => res.send('<h1>Express is working!</h1>'));
 app.listen(3000);
